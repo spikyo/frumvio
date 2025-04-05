56320cedcc8d329a878c21f8f6e3dfda58ebcd69
4rrtem <testqa.artem1111@gmail.com>
2025-04-05 21:15:15 +0300

    initial commit

--- frumvio_project/core/models.py
+++ frumvio_project/core/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db.models import Model, CharField, TextField, DateTimeField, ForeignKey, CASCADE, IntegerField
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Topic(Model):
+    title = CharField(max_length=255)
+    description = TextField(blank=True)
+    created_at = DateTimeField(auto_now_add=True)
+    created_by = ForeignKey(to='User', on_delete=CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class Post(Model):
+    body = TextField()
+    created_at = DateTimeField(auto_now_add=True)
+    topic = ForeignKey(to=Topic, on_delete=CASCADE)
+    created_by = ForeignKey(to='User', on_delete=CASCADE)
+    
+    def __str__(self):
+        return self.body[:30]
